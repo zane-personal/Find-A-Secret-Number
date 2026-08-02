831008bf6d6bec61a8286ed19be1fa78a17b1e51
ZANE <130330643+zane-personal@users.noreply.github.com>
2026-08-02 21:13:57 +0700

    Improve number guessing game with input validation
    Added input validation to ensure a guess is entered before comparing.

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 const secretNum = 7
-let numInput = 0
+
 
 const btn = document.getElementById('btn')
 
 btn.addEventListener('click', function(){
 
+    let numInput = 0
+
     while( numInput != secretNum ) {
         numInput = prompt(' Enter your guess number')
 
-        if(numInput > secretNum) {
+        let numguess = numInput
+
+        if(numguess == '') {
+            alert('Please enter guess number first')
+        }
+        
+        else if(numInput > secretNum) {
             alert('Your Number is greater than Secret Number')
         }
         else if(numInput < secretNum) {
